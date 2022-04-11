67d599569b34b8e79b93168d0d926d5ede3bb461
Brad Sorour <brad.sorour@gmail.com>
2022-04-11 09:49:32 +0100

    resolv

--- networking.rb
+++ networking.rb
@@ -1,7 +1,11 @@
 require 'net/ping'
-
 if Net::Ping::External.new('www.google.com').ping
     puts 'Pong!'
 else
     puts 'No response'
 end
+
+
+# Resolv library is for converting between hostnames and IP addresses
+require 'resolv'
+puts Resolv.getaddress("www.google.com")
