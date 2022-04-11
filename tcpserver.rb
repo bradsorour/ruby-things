4379e0843e96de87c6b5fef4a57b15f18da39db5
Brad Sorour <brad.sorour@gmail.com>
2022-04-11 10:26:20 +0100

    multi-client tcp server

--- tcpserver.rb
+++ tcpserver.rb
@@ -1,11 +1,15 @@
+# Multithreaded tcp server
 require 'socket'
 server = TCPServer.new(1234)
-while connection = server.accept
-  while line = connection.gets
-    break if line =~ /quit/
-    puts line
-    connection.puts "Received!"
-  end
-  connection.puts "Closing the connection. Bye!"
-  connection.close
+
+loop do
+    Thread.start(server.accept) do |connection|
+        while line = connection.gets
+            break if line =~ /quit/
+            puts line
+            connection.puts "Received!"
+        end
+        connection.puts "Closing the connection. Bye!"
+        connection.close
+    end
 end
